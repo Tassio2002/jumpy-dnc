3446e9ef1162f2f30e7db15c19761956504efbab
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-02-12 03:38:32 -0300

    succes message

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,21 +1,33 @@
-let nameInput = document.querySelector("#name-input");
-let requiredMessage = document.querySelectorAll(".required-message");
-let submitBtn = document.querySelector("#submit-btn");
-let inputs = document.querySelectorAll(".input-value");
+const nameInput = document.querySelector("#name-input");
+const requiredMessage = document.querySelectorAll(".required-message");
+const submitBtn = document.querySelector("#submit-btn");
+const inputs = document.querySelectorAll(".input-value");
+const successMessage = document.querySelector("#show-message");
 
 function verifyEmptyInputs() {
+  let i = 0;
+
   requiredMessage.forEach((span) => {
     span.style.display = "none";
   });
-  let i = 0;
+
+  let fieldsFilled = 0
+
   inputs.forEach((input) => {
-    input.value !== "" ? null : showRequiredMessage(i);
+    input.value !== "" ? fieldsFilled++ : showRequiredMessage(i)
+    
+    fieldsFilled !== 5 ? null : showSuccessMessage()
+
     i++;
   });
 }
 
 function showRequiredMessage(inputIndex) {
   requiredMessage[inputIndex].style.display = "block";
+}
+
+function showSuccessMessage() {
+  successMessage.style.display = "block";
 }
 
 submitBtn.addEventListener("click", verifyEmptyInputs);
